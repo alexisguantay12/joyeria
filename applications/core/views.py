8041996de14bf42addd5f0598e7352a257de74c9
alexis <aguantay@hpsca.com.ar>
2025-05-22 09:13:48 -0300

    segundo commit

--- applications/core/views.py
+++ applications/core/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
-
+@login_required
 def home_view(request):
     return render(request,'base.html')
